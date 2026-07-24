b2df82a0f0d26837a9942cfeab5eb4b98bf8901e
gpter <admin@i.pkuschool.edu.cn>
2026-07-24 02:41:47 -0700

    test: bind v44 asset cache version

--- tests/mobile-workbench-layout.test.mjs
+++ tests/mobile-workbench-layout.test.mjs
@@ -10,7 +10,7 @@ const [html, styles, appJs, atlasJs] = await Promise.all([
   readFile(new URL('public/atlas.js', root), 'utf8'),
 ]);
 
-const assetVersion = '20260724v43';
+const assetVersion = '20260724v44';
 
 function block(source, opening, closing = '}') {
   const start = source.indexOf(opening);
